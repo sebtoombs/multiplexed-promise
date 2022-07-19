cc2913d325058259ae794de5c26b72657b761e81
Seb <sebtoombs@gmail.com>
2022-07-19 21:42:37 +1000

    Add options.debug

--- src/index.test.ts
+++ src/index.test.ts
@@ -29,4 +29,53 @@ describe("multiplexedPromise", () => {
 
     expect(promiseGenerator).toHaveBeenCalledTimes(2);
   });
+
+  describe("options.debug", () => {
+    test("Does not log when debug is false", async () => {
+      console.debug = jest.fn();
+
+      const promiseGenerator = jest.fn(() =>
+        Promise.resolve({ key: Symbol() })
+      );
+      const multiplexedPromise = MultiplexedPromise(promiseGenerator);
+
+      const promise1 = multiplexedPromise();
+      const promise2 = multiplexedPromise();
+
+      const result1 = await promise1;
+      const result2 = await promise2;
+
+      expect(console.debug).not.toHaveBeenCalled();
+    });
+    test("Logs to console when debug is true", async () => {
+      console.debug = jest.fn();
+
+      const promiseGenerator = jest.fn(() =>
+        Promise.resolve({ key: Symbol() })
+      );
+      const multiplexedPromise = MultiplexedPromise(promiseGenerator, {
+        debug: true,
+      });
+
+      const promise1 = multiplexedPromise();
+      const promise2 = multiplexedPromise();
+
+      const result1 = await promise1;
+      const result2 = await promise2;
+
+      expect(console.debug).toHaveBeenCalledTimes(3);
+      // @ts-ignore
+      expect(console.debug.mock.calls[0][1]).toMatch(
+        /no pendingPromise found, creating a new one/
+      );
+      // @ts-ignore
+      expect(console.debug.mock.calls[1][1]).toMatch(
+        /pendingPromise found, waiting for it to resolve/
+      );
+      // @ts-ignore
+      expect(console.debug.mock.calls[2][1]).toMatch(
+        /promise resolved, removing from cache/
+      );
+    });
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,40 @@
 const promiseCache = new Map();
 
-export interface Options {}
+export interface Options {
+  debug?: boolean;
+}
 
 const MultiplexedPromise = (
   promiseGenerator: (...args: any[]) => Promise<any>,
   // eslint-disable-next-line
   options?: Options
 ) => {
+  const { debug = false } = options || {};
+
+  const ts = () => {
+    const now = new Date();
+    return `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}.${now.getMilliseconds()}`;
+  };
+  const _debug = (...args) =>
+    !!debug &&
+    console.debug.call(console, `[${ts()}] MultiplexedPromise:`, ...args);
+
   const key = Symbol("promise-key");
 
   return async (...args: any[]): Promise<any> => {
     const pendingPromise = promiseCache.get(key);
     if (pendingPromise) {
+      _debug("pendingPromise found, waiting for it to resolve");
       return pendingPromise;
     }
 
+    _debug("no pendingPromise found, creating a new one");
     const promise = promiseGenerator(...args);
 
     promiseCache.set(key, promise);
 
     return promise.then((r) => {
+      _debug("promise resolved, removing from cache");
       promiseCache.delete(key);
       return r;
     });
